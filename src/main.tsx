d6ab50f2086d3c73794035cc0e20817fcdc56e35
DanielMar82 <danielmarruiz288@gmail.com>
2025-12-15 17:52:54 +0100

    cambios pruebas

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Frame from "./components/Frame/Frame.tsx";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 
